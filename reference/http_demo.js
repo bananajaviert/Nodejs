fa097328d3daf256fe5ffccd65902d42cf236998
Joe <=>
2021-06-08 23:06:44 +0800

    Nodejs first app

--- reference/http_demo.js
+++ reference/http_demo.js
@@ -1,22 +1,22 @@
-import http from 'http'
+// import http from 'http'
 
-const port = 8080
+// const port = 8080
 
-// Create Server obj
+// // Create Server obj
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.write('Welcome!')
-        res.end()
-    } else if (req.url === '/groups/izone') {
-        res.write('This is IZ*ONE!')
-        res.end()
-    } else if (req.url === '/groups/loona') {
-        res.write('This is LOONA!')
-        res.end()
-    } else if (req.url === `/groups/cherry%20bullet`) {
-        res.write('This is Cherry Bullet!')
-        res.end()
-    }
-}).listen(port, () => { console.log('Listening on port', port) })
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/') {
+//         res.write('Welcome!')
+//         res.end()
+//     } else if (req.url === '/groups/izone') {
+//         res.write('This is IZ*ONE!')
+//         res.end()
+//     } else if (req.url === '/groups/loona') {
+//         res.write('This is LOONA!')
+//         res.end()
+//     } else if (req.url === `/groups/cherry%20bullet`) {
+//         res.write('This is Cherry Bullet!')
+//         res.end()
+//     }
+// }).listen(port, () => { console.log('Listening on port', port) })
 
